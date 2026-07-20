bd3e4b1da37563bf6c49b3a49d78c27b7f92a115
Ayush Kumar <vermaayush84300@gmail.com>
2026-07-20 22:43:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let x = document.getElementById("heading");
+x.innerText = "Welcome to the FSD Workshop!";
+x.addEventListener("click", function () {
+    x.style.color = "red";
+    x.style.fontSize = "50px";
+});
